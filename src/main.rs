f9c869ed809b5b0f3353c09230c006b4fcd3f296
Felipe Balbi <felipe.balbi@microsoft.com>
2026-02-19 12:14:08 -0800

    Extract peripherals too

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
 use anyhow::Result;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
+use std::num::ParseIntError;
 use std::{fs::File, io::BufReader};
 
 fn main() -> Result<()> {
     let mut data = Metadata::default();
 
-    let pinout = BufReader::new(File::open("data/mcxa2/pinout.csv").expect("cannot open file"));
-    let mut rdr = csv::Reader::from_reader(pinout);
+    let pinout = BufReader::new(File::open("data/mcxa2/pinout.csv")?);
+    let mut pinout_rdr = csv::Reader::from_reader(pinout);
 
-    let all_records = rdr
-        .deserialize()
+    let memory_map = BufReader::new(File::open("data/mcxa2/memory-map.csv")?);
+    let mut memory_map_rdr = csv::Reader::from_reader(memory_map);
+    let all_peripherals = memory_map_rdr
+        .deserialize::<MemoryMapRecord>()
         .filter(|r| r.is_ok())
-        .map(|r| r.unwrap())
-        .collect::<Vec<Record>>();
+        .map(|r| r.unwrap().instance.to_uppercase())
+        .filter(|name| !name.is_empty())
+        .map(|name| Peripheral::new(name))
+        .collect::<Vec<_>>();
+
+    data.peripherals = all_peripherals;
 
     // Iterate over the data and extract all valid ALT0 and I/O SUPPLY
-    for record in all_records {
+    for record in pinout_rdr
+        .deserialize::<PinoutRecord>()
+        .filter(|r| r.is_ok())
+        .map(|r| r.unwrap())
+    {
         let supply = record.supply;
         let gpio = record.alt0;
 
@@ -31,9 +42,44 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+/// Custom function to deserialize a hex string into a u32
+fn from_hex<'de, D>(deserializer: D) -> std::result::Result<u32, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: &str = Deserialize::deserialize(deserializer)?;
+    let s = s.trim();
+
+    // Remove optional "0x" or "0X" prefix
+    let hex_str = s
+        .strip_prefix("0x")
+        .or_else(|| s.strip_prefix("0X"))
+        .unwrap_or(s);
+
+    u32::from_str_radix(hex_str, 16)
+        .map_err(|e: ParseIntError| serde::de::Error::custom(format!("Invalid hex: {}", e)))
+}
+
 #[derive(Debug, Deserialize)]
 #[allow(dead_code)]
-struct Record {
+struct MemoryMapRecord {
+    #[serde(rename = "Peripheral description")]
+    desc: String,
+    #[serde(rename = "module_nickname")]
+    nickname: String,
+    #[serde(rename = "Peripheral instance")]
+    instance: String,
+    #[serde(rename = "Size (KB)")]
+    size: u32,
+    #[serde(rename = "Start address (hex)", deserialize_with = "from_hex")]
+    start: u32,
+    #[serde(rename = "End address (hex)", deserialize_with = "from_hex")]
+    end: u32,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+struct PinoutRecord {
     #[serde(rename = "MCXA26x/A25x/A18x/A17x\nLQFP144")]
     lqfp144_pin_number: Option<u32>,
     #[serde(rename = "MCXA26x/A25x/A18x/A17x\n LQFP144 Pin Name")]
@@ -99,17 +145,23 @@ struct Metadata {
     // Should, somehow, be extracted from vendor data files
     chips: Vec<String>,
     pins: Vec<Pin>,
-    // todo
-    // peripherals: Vec<Peripheral>,
+    // TODO: Complete signal table
+    peripherals: Vec<Peripheral>,
 }
 
 impl Metadata {
-    fn new(comment: String, chips: Vec<String>, pins: Vec<Pin>) -> Self {
+    fn new(
+        comment: String,
+        chips: Vec<String>,
+        pins: Vec<Pin>,
+        peripherals: Vec<Peripheral>,
+    ) -> Self {
         Self {
             schema: "./schema.json".to_string(),
             comment,
             chips,
             pins,
+            peripherals,
         }
     }
 }
@@ -153,6 +205,7 @@ impl Default for Metadata {
                 "MCXA266VLQ".to_string(),
             ],
             vec![],
+            vec![],
         )
     }
 }
@@ -167,4 +220,25 @@ impl Pin {
     fn new(name: String, supply: String) -> Self {
         Self { name, supply }
     }
+}
+
+#[derive(Serialize)]
+struct Peripheral {
+    name: String,
+    signals: Vec<Signal>,
+}
+
+impl Peripheral {
+    fn new(name: String) -> Self {
+        Self {
+            name,
+            signals: vec![],
+        }
+    }
+}
+
+#[derive(Serialize)]
+struct Signal {
+    name: String,
+    pins: Vec<Pin>,
 }
